80f0f1803da91534d23a05e3093240be89a1840e
Arief Novianto <arief.novianto8@gmail.com>
2018-05-04 17:23:21 +0700

    add favicon

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Organization Member</title>
+		<link rel="icon" href="/favicon/16x16.ico" size="16x16">
+		<link rel="icon" href="/favicon/32x32.ico" size="32x32">
+		<link rel="icon" href="/favicon/48x48.ico" size="48x48">
+	</head>
+	<body>
+		Hellow
+	</body>
+</html>
