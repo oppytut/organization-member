312209dead65694aa5152781bba79b5b1fad5fce
Arief Novianto <arief.novianto8@gmail.com>
2018-05-05 14:42:34 +0700

    change home name

---
+++ resources/views/listMember.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Kepo, ih! -->
+		<meta charset="utf-8">
+		<title>Organization Member's</title>
+
+		<!-- Favicon -->
+		<link rel="icon" href="/favicon/16x16.ico" size="16x16">
+		<link rel="icon" href="/favicon/32x32.ico" size="32x32">
+		<link rel="icon" href="/favicon/48x48.ico" size="48x48">
+
+		<!-- CSS -->
+		<link rel="stylesheet" href="/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/main.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-light" style="background-color: #FF9200	;">
+			<a class="navbar-brand" href="#" style="color: #FFE5C3; font-size: 16px;">
+				<img src="/icon/24x24-white.png" alt="">
+				<span class="align-middle">
+					Organization Member's
+				</span>
+			</a>
+		</nav>
+
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<table class="table">
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">First</th>
+								<th scope="col">Last</th>
+								<th scope="col">Handle</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<th scope="row">1</th>
+								<td>Mark</td>
+								<td>Otto</td>
+								<td>@mdo</td>
+							</tr>
+							<tr>
+								<th scope="row">2</th>
+								<td>Jacob</td>
+								<td>Thornton</td>
+								<td>@fat</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>Larry</td>
+								<td>the Bird</td>
+								<td>@twitter</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+
+		<!-- JavaScript (influential sequence) -->
+		<script src="/js/jquery-3.3.1.slim.min.js"></script>
+		<script src="/js/popper.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
+	</body>
+</html>
