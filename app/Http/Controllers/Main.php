93cf389c4c8ec9bb23f6616e3b0f51154febb091
Arief Novianto <arief.novianto8@gmail.com>
2018-05-06 15:44:19 +0700

    retrieve data from database

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -3,10 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+// Model
+use App\Member;
 
 class Main extends Controller
 {
 	public function __invoke() {
-		return view('listMember');
+		// Retrieving all members
+		$members = Member::all();
+
+		// Retriving all member column name
+		$member_columns = DB::getSchemaBuilder()->getColumnListing('members');
+
+		// Deleting sensitive column name
+		foreach($member_columns as $key => $column_name) {
+			if(in_array($column_name, [
+				'id',
+				'created_at',
+				'updated_at',
+				'deleted_at'
+			])) {
+				unset($member_columns[$key]);
+			}
+		}
+
+		// Create readable member column
+		$readable_member_columns = $member_columns;
+
+		foreach ($readable_member_columns as $key => $column_name) {
+			$readable_member_columns[$key] = ucwords(str_replace('_', ' ', $readable_member_columns[$key]));
+		}
+
+		return view('listMember', [
+			'members' => $members,
+			'member_columns' => $member_columns,
+			'readable_member_columns' => $readable_member_columns
+		]);
 	}
 }
